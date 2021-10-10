a6a31dfe91d9b70c892489bc7b3146b2aea7843f
Volkorp <jesusbernalley@gmail.com>
2021-10-10 11:32:03 +0200

    GHP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { MatSliderModule } from '@angular/material/slider';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatSliderModule,
+    MatProgressBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
